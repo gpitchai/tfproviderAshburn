3b487d958056e50781311de5215f6253cc9fb255
Ganesh Pitchaiah <35233229+gpitchai@users.noreply.github.com>
2022-10-11 11:12:51 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,19 @@
+// Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+    }
+  }
+}
+
 provider "oci" {
-  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaplkmid2untpzjcxrmbv4nowe74yb4lr6idtckwo4wyf7jh23be4q"
-  user_ocid = "ocid1.user.oc1..aaaaaaaaft7ewogmwla6zoah4gpapcffx4vtaqvhrurv3axosfiqjs434plq" 
-  private_key_path = "$HOME/.oci/terraform.pem"
-  fingerprint = "20:ef:a0:cc:50:27:46:56:f3:17:36:19:53:25:1a:61"
   region = "us-ashburn-1"
+  # tenancy_ocid     = var.tenancy_ocid
+  # fingerprint      = var.fingerprint
+  # user_ocid        = var.user_ocid
+  # private_key_path = var.private_key_path
 }
